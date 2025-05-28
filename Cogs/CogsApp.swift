fbb357ff2dfd91da13f50630c790b5b5b10f40e1
ghabrielferrarii <ghcferrari75@gmail.com>
2025-05-28 08:58:58 -0300

    Initial Commit

---
+++ Cogs/CogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CogsApp.swift
+//  Cogs
+//
+//  Created by Aluno 17 on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cogs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cogs
+//
+//  Created by Aluno 17 on 28/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
