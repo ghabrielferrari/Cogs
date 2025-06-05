0433188a170a630a7b21f517c277bdd81b587688
Felipe <felipealbertosantos@outlook.com>
2025-06-05 16:03:40 -0300

    tests with Core Data

--- Cogs/Views/ContentView.swift
+++ Cogs/Views/ContentView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+@State private var Tag: String = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("This is a test Core Data App!")
+                .padding()
+            HStack{
+                TextField("Tag name", text: $Tag)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                Button(action:{
+                    saveTag()
+                }){
+                    Image(systemName: "plus.circle.fill")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(60)
+                }
+                .disabled(Tag.isEmpty)
+                .padding()
+            }
+            
+        }
     }
+}
+
+private func saveTag(){
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
